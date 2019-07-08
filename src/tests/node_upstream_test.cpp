058a035068f3d91f9a76e5642a1fd548a3badf17
Kevin Morris <kevr.gtalk@gmail.com>
2019-07-08 01:03:31 -0700

    auth exchange in node_upstream_test
    this sleep method needs to be fixed: perhaps we will
    provide testing helpers that allow us to wait until
    clients or servers handshake etc
    
    Signed-off-by: Kevin Morris <kevr.gtalk@gmail.com>

--- src/tests/node_upstream_test.cpp
+++ src/tests/node_upstream_test.cpp
@@ -2,10 +2,34 @@
 #include "node/upstream.hpp"
 #include <gtest/gtest.h>
 
+using namespace ns;
+
 class node_upstream_test : public ::testing::Test
 {
+protected:
+    virtual void SetUp()
+    {
+        set_trace_logging(true);
+        trace();
+        m_server.use_certificate("cert.crt", "cert.key").start();
+    }
+
+    virtual void TearDown()
+    {
+        trace();
+        m_server.stop();
+    }
+
+    host::node_server m_server{6666};
 };
 
 TEST_F(node_upstream_test, auth_works)
 {
+    auto upstream = std::make_shared<node::upstream>(m_server.get_io_service());
+    upstream->on_connect([this](auto client) {
+        logi("sending auth key abcd");
+        client->auth("abcd");
+    });
+    upstream->run("localhost", "6666");
+    std::this_thread::sleep_for(std::chrono::seconds(10));
 }
