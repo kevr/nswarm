c6e0383ea216ba57f3d9d287ee080fa87dbec727
Kevin Morris <kevr.gtalk@gmail.com>
2019-08-18 20:38:46 -0700

    scripts: coverage now removes external results
    Signed-off-by: Kevin Morris <kevr.gtalk@gmail.com>

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -6,5 +6,13 @@ rm -f cov.info
 
 # Generate coverage report
 lcov -c --directory . --output-file cov.info
-genhtml cov.info --output-directory report
+
+# Filter out unwanted report sources
+lcov --remove cov.info -o cov.filtered.info \
+    '*json.hpp*' \
+    '*boost*' \
+    '/usr/include*' \
+    '9.1.0*'
+
+genhtml cov.filtered.info --output-directory report
 
